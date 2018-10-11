39d698e06882cb8b0c434d8173103258cb839732
jlt <joseluistorrehernandez@gmail.com>
2018-10-12 01:52:01 +0200

    modificado:     cat.rb
        Se han añadido comentarios con el fin de explicar la funcionalidad del ejemplo

--- cat.rb
+++ cat.rb
@@ -1,1 +1,10 @@
+# cat.rb 
 $stdin.each {|linea| $stdout.puts linea}
+
+# en este sencillo ejemplo (tomado de Carlson y Richardson) 
+# vemos la utilización de las variables globales de Ruby,
+# las cuales deben empezar con el símbolo de $
+
+# En este caso se trata de las variables $stdin
+# y $stdout, que representan la entrada y la salida estándar.
+#
