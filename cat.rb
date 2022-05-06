ee1d7f4da483123dbb6102ee98ce38408472f688
José Luis Torre Hernández <joseluistorrehernandez@gmail.com>
2022-05-07 01:18:45 +0200

    Update cat.rb

--- cat.rb
+++ cat.rb
@@ -1,10 +1,11 @@
 # cat.rb 
 $stdin.each {|linea| $stdout.puts linea}
 
-# en este sencillo ejemplo (tomado de Carlson y Richardson) 
+# En este sencillo ejemplo (tomado de Carlson y Richardson) 
 # vemos la utilización de las variables globales de Ruby,
 # las cuales deben empezar con el símbolo de $
 
 # En este caso se trata de las variables $stdin
-# y $stdout, que representan la entrada y la salida estándar.
+# y $stdout, que representan la entrada y la salida estándar
+# y son dos de las variables predefinidas en ruby.
 #
