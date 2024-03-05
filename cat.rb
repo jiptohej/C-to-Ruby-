2928271d404eb336184b6f74e4df7b64dda16a2b
José Luis Torre Hernández <joseluistorrehernandez@gmail.com>
2024-03-05 19:09:34 +0100

    Update cat.rb
    Simple modificación de estilo en la escritura

--- cat.rb
+++ cat.rb
@@ -3,7 +3,7 @@ $stdin.each {|linea| $stdout.puts linea}
 
 # En este sencillo ejemplo (tomado de Carlson y Richardson) 
 # vemos la utilización de las variables globales de Ruby,
-# las cuales deben empezar con el símbolo de $
+# las cuales deben empezar con el símbolo: $
 
 # En este caso se trata de las variables $stdin
 # y $stdout, que representan la entrada y la salida estándar
